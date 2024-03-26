d2d27dc24f4f200aa73e21376309b8b67b041534
e <jannatul0040@gmail.com>
2024-03-26 22:26:36 +0600

    modified nav section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,107 +1,82 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import { IoCartOutline } from "react-icons/io5";
+import { useState } from 'react';
+import { MdMenu } from "react-icons/md";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-//     <div className="nav-section">
-//     <nav className="nav-container">
-//         <h1 className='nav-logo'>Taste now</h1>
-//         <ul className="nav-link">
-//             <li>
-//                 <NavLink
-//                     to="/"
-//                     className={({ isActive, isPending }) =>
-//                         isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold " : ""
-//                     }
-//                 >
-//                     Home
-//                 </NavLink>
-//             </li>
-//             <li>
-//                 <NavLink
-//                     to="/donation"
-//                     className={({ isActive, isPending }) =>
-//                         isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold " : ""
-//                     }
-//                 >
-//                     Donation
-//                 </NavLink>
-//             </li>
-//             <li>
-//                 <NavLink
-//                     to="/statistics"
-//                     className={({ isActive, isPending }) =>
-//                         isPending ? "pending" : isActive ? "text-[#FF444A] underline font-bold " : ""
-//                     }
-//                 >
-//                     Statistics
-//                 </NavLink>
-//             </li>
-//         </ul>
-//     </nav>
-// </div>
-<div className="nav-section">
-  <nav className="nav-container">
-    <h1 className='nav-logo'>Taste now</h1>
-    <ul className="nav-link">
-      <li>
-        <NavLink
-          exact
-          to="/"
-          activeClassName="active"
-          className="nav-item"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/about-us"
-          activeClassName="active"
-          className="nav-item"
-        >
-          About us
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/blog"
-          activeClassName="active"
-          className="nav-item"
-        >
-          Blog
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/contact"
-          activeClassName="active"
-          className="nav-item"
-        >
-          Contact Us
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/cart"
-          activeClassName="active"
-          className="nav-item"
-        >
-          <IoCartOutline className='nav-item-cart' />
-        </NavLink>
-      </li>
-      <p>
-        <NavLink
-          to="/contact"
-          className="signup-button"
-        >
-          Contact Us
-        </NavLink>
-      </p>
-    </ul>
-  </nav>
-</div>
+    <div className="nav-section">
+      <nav className="nav-container">
+        <div className="nav-left">
+          <div className="menu-icon" onClick={toggleMenu}>
+            <MdMenu />
+          </div>
+          <h1 className="nav-logo">Taste now</h1>
+        </div>
+        <ul className={`nav-link ${showMenu ? 'show-menu' : ''}`}>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              className="nav-item"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us"
+              activeClassName="active"
+              className="nav-item"
+            >
+              About us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              activeClassName="active"
+              className="nav-item"
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              activeClassName="active"
+              className="nav-item"
+            >
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-right">
+          <NavLink
+            to="/cart"
+            activeClassName="active"
+            className="nav-item"
+          >
+            <IoCartOutline className="nav-item-cart" />
+          </NavLink>
+          <p>
+            <NavLink
+              to="/sign-up"
+              className="signup-button"
+            >
+              Sign Up
+            </NavLink>
+          </p>
+        </div>
+      </nav>
+    </div>
   );
 }
 
